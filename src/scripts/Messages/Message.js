d0d6164545f936705788df13965b9820f488edf7
Nima-ShSh <nimashsh21@gmail.com>
2021-11-24 11:46:55 -0500

    a

--- src/scripts/Messages/Message.js
+++ src/scripts/Messages/Message.js
@@ -1,21 +1,21 @@
-// import { deleteMessage } from "./MessageProvider.js" no deleteMessage in Provider
-import { MessageList } from "./MessageList.js"
-import { MessageEditForm } from "./MessageEditForm.js"
+// // import { deleteMessage } from "./MessageProvider.js" no deleteMessage in Provider
+// import { MessageList } from "./MessageList.js"
+// import { MessageEditForm } from "./MessageEditForm.js"
 
-export const messages = (messageObject) => {
-    return `<section class = "messageCard">
-    <div id="id">ID: </div> //this line would be for chat uses
-    <div class="DateOfMessage"><a>Date: </a>${messageObject.DateOfMessage}</div>
-    <div class="messageText">Message: ${messageObject.messageText}</div>
-    <div class="chatUser>Chat User: ${messageObject.chatUser}</div>
-    <button id="deleteMessage-- ${messageObject.id}">Delete</button>
-    <button id="edit--${messageObject.id}">Edit</button>
-    </section>`
-}
+// export const messages = (messageObject) => {
+//     return `<section class = "messageCard">
+//     <div id="id">ID: </div> //this line would be for chat uses
+//     <div class="DateOfMessage"><a>Date: </a>${messageObject.DateOfMessage}</div>
+//     <div class="messageText">Message: ${messageObject.messageText}</div>
+//     <div class="chatUser>Chat User: ${messageObject.chatUser}</div>
+//     <button id="deleteMessage-- ${messageObject.id}">Delete</button>
+//     <button id="edit--${messageObject.id}">Edit</button>
+//     </section>`
+// }
 
-const eventHub = document.querySelector("#chat-list")
-eventHub.addEventListener("click", (eventObject) => {
+// const eventHub = document.querySelector("#chat-list")
+// eventHub.addEventListener("click", (eventObject) => {
 
-    const noteId = +eventObject.target.id.split("--")[1]
-    MessageEditForm(messageId);
-})
+//     const noteId = +eventObject.target.id.split("--")[1]
+//     MessageEditForm(messageId);
+// })
--- src/scripts/Messages/MessageEditForm.js
+++ src/scripts/Messages/MessageEditForm.js
@@ -1,9 +1,9 @@
-import {useMessages} from "./MessageProvider.js"
+import {useMessages, updateMessage} from "./MessageProvider.js"
+import { MessageList } from "./MessageList.js"
 // import { updateMessage} from "./MessageProvider.js"
-// import { MessageList } from "./MessageList.js"
 
 // We're going to print the edit form where the "add note" form usually goes. We could move it around on the page by changing our content target.
-const contentTarget = document.querySelector("#chat-list")
+
 
 export const MessageEditForm = (messageId) => {
     // Give this component access to our application's notes state
@@ -11,14 +11,31 @@ export const MessageEditForm = (messageId) => {
 
     // Find the note that we clicked on by its unique id
     const messageWeWantToEdit = allmessages.find(singleMessage=> singleMessage.id === messageId)
+    const contentTarget = document.querySelector(`#messageId--${messageId}`)
 
     // Print the form
     // We'll use the HTML value attribute to pre-populate our form fields with the note's info
     contentTarget.innerHTML = `
+    <section class="messageList-edit">
         <h2>Edit Message</h2>
-        <input type="date" id="message-date" value="${messageWeWantToEdit.dateOfMessage}" />
-        <input type="text" value="${messageWeWantToEdit.messageText}" id="message-text" />
-        <input type="text" value="${messageWeWantToEdit.chatUser}" id="message - chatUser" />
-        <button id="saveNoteChanges-${messageId}">Save Changes</button>
+        <input type="date" id="message-date-edit" value="${messageWeWantToEdit.dateOfMessage}"/>
+        <input type="text" value="${messageWeWantToEdit.messageText}" id="message-text-edit"/>
+        <button id="editMessage--${messageId}">Save Changes</button>
+    </section>
     `
 }
+
+document.querySelector("#chat-list").addEventListener("click", (event) => {
+    if(event.target.id.startsWith("editMessage")){
+        const editedMessage = {
+            id: +event.target.id.split("--")[1],
+            messageText: document.querySelector("#message-text-edit").value,
+            dateOfMessage: document.querySelector("#message-date-edit").value,
+            userId: +sessionStorage.getItem("activeUser")
+        }
+        // console.log(editedMessage)
+        updateMessage(editedMessage)
+        .then(MessageList)
+    }
+})
+
